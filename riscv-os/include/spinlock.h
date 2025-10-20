bd29a80f41aac1d872330deac0876cb2cc6dbb61
Zhanng-Whu <1552323699@qq.com>
2025-10-21 01:39:23 +0800

    <test>

--- riscv-os/include/spinlock.h
+++
@@ -1,9 +0,0 @@
-#ifndef SPINLOCK
-#define SPINLOCK
-struct spinlock{
-    uint locked;
-    char* name;
-    struct cpu* cpu;
-};
-
-#endif
--- riscv-os/user/test.c
+++
@@ -1,6 +0,0 @@
-#include "include/types.h"
-#include "include/user.h"
-int main(int argc, char *argv[]){
-    hello();
-    return 0;
-}
